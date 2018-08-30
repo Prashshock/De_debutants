f96ad5f7998db013b01a0fbeece6e463431e44de
Prashshock_ <prashshock@gmail.com>
2018-08-30 16:17:06 +0530

    first

--- app/src/main/java/com/example/prashshock/ui_project/AllActivity.java
+++ app/src/main/java/com/example/prashshock/ui_project/AllActivity.java
@@ -3,11 +3,11 @@ package com.example.prashshock.ui_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HouseHold extends AppCompatActivity {
+public class AllActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_house_hold);
+        setContentView(R.layout.activity_all);
     }
 }
